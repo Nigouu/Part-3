aef95797f45b7c48c1893d37278731f6940af168
Nigouu <uggla.nicolas@gmail.com>
2021-08-17 12:52:15 +0300

    exercise3.5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     { 
@@ -52,6 +53,30 @@ app.get('/api/persons', (request, response) => {
     response.status(204).end()
   })
 
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+    persons = persons.concat(person)
+    response.json(person)
+  })
+
+  
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
