d15b0854057ee753f9cfba4999429db845f85d0e
Nigouu <uggla.nicolas@gmail.com>
2021-08-17 14:14:12 +0300

    exercise3.8

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.json())
 //   app.use(unknownEndpoint)
 
   const morgan = require('morgan')
-  app.use(morgan('tiny'))
+  morgan.token('body', (req, res) => JSON.stringify(req.body));
+  app.use(morgan(':method :url :status :response-time ms :body - '))
   
 let persons = [
     { 
