a751f088fd7ea46df3718d444990acadd4140ca3
Nigouu <uggla.nicolas@gmail.com>
2021-08-23 12:46:45 +0300

    ojerng

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 const express = require('express')
 const app = express()
-app.use(express.json())
-
-// const requestLogger = (request, response, next) => {
-//     console.log('Method:', request.method)
-//     console.log('Path:  ', request.path)
-//     console.log('Body:  ', request.body)
-//     console.log('---')
-//     next()
-//   }
-//   app.use(requestLogger)
-
-//   const unknownEndpoint = (request, response) => {
-//     response.status(404).send({ error: 'unknown endpoint' })
-//   }
-//   app.use(unknownEndpoint)
-
-  const morgan = require('morgan')
+const cors = require('cors')
+const morgan = require('morgan')
   morgan.token('body', (req, res) => JSON.stringify(req.body));
-  app.use(morgan(':method :url :status :response-time ms :body - '))
-  
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan(':method :url :status :response-time ms :body - '))
+
 let persons = [
     { 
       "id": 1,
@@ -95,6 +83,7 @@ app.get('/api/persons', (request, response) => {
   })
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
