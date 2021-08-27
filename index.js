54cb31a923b401e6f03b5c2d9e11e265494e9a35
Nigouu <uggla.nicolas@gmail.com>
2021-08-27 12:14:50 +0300

    yessboi

--- index.js
+++ index.js
@@ -22,33 +22,34 @@ app.use(morgan(':method :url :status :response-time ms :body - '))
     console.error(error.message)
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
     next(error)
   }
 
-
-// let persons = [
-//     { 
-//       "id": 1,
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": 2,
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": 3,
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": 4,
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ]
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
 
 
 app.get('/api/persons', (request, response) => {
@@ -106,7 +107,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     return maxId + 1
   }
 
-  app.post('/api/persons', (request, response) => {
+  app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (body.name === undefined) {
       return response.status(400).json({ error: 'content missing' })
@@ -116,9 +117,13 @@ app.put('/api/persons/:id', (request, response, next) => {
       number: body.number,
       id: generateId(),
     })
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
+    person
+      .save()
+      .then(savedNote => savedNote.toJSON())
+      .then(savedAndFormattedPerson => {
+        response.json(savedAndFormattedPerson)
+      }) 
+    .catch(error => next(error))
   })
 
   app.use(unknownEndpoint)
